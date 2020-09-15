7fe3dec1f073a9c05f7678fa38b8907c1684de44
João Correia <jcjunior.contatos@gmail.com>
2020-09-14 23:04:49 -0300

    Update exe4.4.py
    NÃO CONCLUÍDO - falha na interrupção do programa.

--- exe4.4.py
+++ exe4.4.py
@@ -2,4 +2,7 @@ print()
 print('4.4 – Um milhão:\n Crie uma lista de números de um a um milhão e, então, use um laço for para exibir os números. (Se a saída estiver demorando demais, interrompa pressionando CTRL-C ou feche a janela de saída.)')
 print()
 for valor in range(1,1000001):
-    print(valor)
+    try:
+        print(valor)
+    except (KeyboardInterrupt):  
+        print('Você pressionou Ctrl+C ou Delete para interromper este programa!')
