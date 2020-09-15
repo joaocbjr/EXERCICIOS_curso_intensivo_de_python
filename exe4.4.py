0f980e8281c30a311ec476c2958d9595b849f025
João Correia <jcjunior.contatos@gmail.com>
2020-09-14 22:45:10 -0300

    Create exe4.4.py

---
+++ exe4.4.py
@@ -0,0 +1,5 @@
+print()
+print('4.4 – Um milhão:\n Crie uma lista de números de um a um milhão e, então, use um laço for para exibir os números. (Se a saída estiver demorando demais, interrompa pressionando CTRL-C ou feche a janela de saída.)')
+print()
+for valor in range(1,1000001):
+    print(valor)
