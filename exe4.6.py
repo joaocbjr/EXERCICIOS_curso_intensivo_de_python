6607dcb12dc3aa675a0d4d3a0bf99b883d642f9b
João Correia <jcjunior.contatos@gmail.com>
2020-09-15 00:23:47 -0300

    Create exe4.6.py
    Números Ímpares

---
+++ exe4.6.py
@@ -0,0 +1,9 @@
+print()
+print('4.6 – Números ímpares:\n Use o terceiro argumento da função range() para criar uma lista de números ímpares de 1 a 20. Utilize um laço for para exibir todos os números.')
+print()
+impar = list(range(1,21,2))
+print('Os números ímpares da lista: ',impar)
+print('Todos os números da lista são: ')
+for todo in range(1,21):
+    print(todo)
+
