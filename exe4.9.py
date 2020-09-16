3bafd52a433fffc22da74788d848c91d52708471
João Correia <jcjunior.contatos@gmail.com>
2020-09-15 23:28:45 -0300

    Create exe4.9.py
    Comprehension de cubos

---
+++ exe4.9.py
@@ -0,0 +1,5 @@
+print()
+print('4.9 – Comprehension de cubos:\n Use uma list comprehension para gerar uma lista dos dez primeiros cubos.')
+print()
+cubos= [value**3 for value in range(1,11)]
+print(cubos)
