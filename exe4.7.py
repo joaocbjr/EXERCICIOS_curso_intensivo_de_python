99166eb21902a09df16a705fd9886d05bba9d6dd
João Correia <jcjunior.contatos@gmail.com>
2020-09-15 23:28:06 -0300

    Create exe4.7.py

---
+++ exe4.7.py
@@ -0,0 +1,5 @@
+print()
+print('4.7– Três:\n Crie uma lista de múltiplos de 3, de 3 a 30. Use um laço for para exibir os números de sua lista.')
+print()
+tres = list(range(3,33,3))
+print('Os números multiplos de 03 da lista: ',tres)
