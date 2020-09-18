7a13b302122628139ea13182ec6bc278abe9269f
João Correia <jcjunior.contatos@gmail.com>
2020-09-17 23:03:23 -0300

    Update exe4.1.py
    Total de convidados

--- exe4.1.py
+++ exe4.1.py
@@ -1,5 +1,10 @@
 print()
 print('3.9  –  Convidados  para  o  jantar: Trabalhando  com  um  dos  programas  dos Exercícios  de  3.4  a  3.7  (páginas  80  e  81),  use len()  para  exibir  uma mensagem informando o número de pessoas que você está convidando para o jantar.')
 print()
-
-
+#Parte do código do exe3.6
+convidado = ['Vovó', 'Mãe', 'Pai', 'Bernardo']
+convidado.insert(1, 'Ester')
+convidado.insert(4, 'Dona Chica')
+convidado.append('Roberto')
+print('A lista de convidados é:\n',convidado)
+print('Total de convidados: ',len(convidado))
