f7d62449d2d4ff903619b0d1b676f49e95a59cde
João Correia <jcjunior.contatos@gmail.com>
2020-09-17 22:43:32 -0300

    Create exe4.11.py

---
+++ exe4.11.py
@@ -0,0 +1,3 @@
+print()
+print('4.11 – Minhas pizzas, suas pizzas:\n Comece com seu programa do Exercício 4.1(página 97). Faça uma cópia da lista de pizzas e chame-a de friend_pizzas. Então faça o seguinte: \n• Adicione uma nova pizza à lista original.\n• Adicione uma pizza diferente à lista friend_pizzas.\n• Prove que você tem duas listas diferentes. Exiba a mensagem Minhas pizzas favoritas  são:;  em  seguida,  utilize  um  laço for  para  exibir  a  primeira  lista. Exiba a mensagem As pizzas favoritas de meu amigo são:; em seguida, utilize um laço for para exibir a segunda lista. Certifique-se de que cada pizza nova esteja armazenada na lista apropriada.')
+print()
