59f9506dcfce500e3accc184e76199a170f2a291
João Correia <jcjunior.contatos@gmail.com>
2020-09-20 12:26:53 -0300

    Update exe4.1.py

--- exe4.1.py
+++ exe4.1.py
@@ -1,3 +1,9 @@
 print()
 print('4.1 – Pizzas:\n Pense em pelo menos três tipos de pizzas favoritas. Armazene os nomes dessas pizzas e, então, utilize um laço for para exibir o nome de cada pizza.\n• Modifique seu laço for para mostrar uma frase usando o nome da pizza em vez de exibir apenas o nome dela. Para cada pizza, você deve ter uma linha na saída contendo uma frase simples como Gosto de pizza de pepperoni.\n• Acrescente uma linha no final de seu programa, fora do laço for, que informe quanto você gosta de pizza. A saída deve ser constituída de três ou mais linhas sobre os tipos de pizza que você gosta e de uma frase adicional, por exemplo, Eu realmente adoro pizza')
 print()
+pizzas = ['napolitano', '4 queijos', 'margerita']
+
+for pizza in pizzas:
+    print('Gosto de pizza',pizza)
+print()
+print('Pizza é bom de mais, melhor que uma só duas!')
