6a1baa3a62a078a3536f24b96ddc1bf05aa09703
João Correia <jcjunior.contatos@gmail.com>
2020-09-21 00:15:02 -0300

    Create exe4.12.py

---
+++ exe4.12.py
@@ -0,0 +1,9 @@
+print()
+print('4.12 – Mais laços:\n Todas as versões de foods.py nesta seção evitaram usar laços for para fazer exibições a fim de economizar espaço. Escolha uma versão de foods.py e escreva dois laços for para exibir cada lista de comidas.')
+print()
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+
+
+for my in my_foods:
+    print(my)
