903aa264cec390f9013ab3d9e41d3a21593eb180
João Correia <jcjunior.contatos@gmail.com>
2020-09-21 21:49:16 -0300

    Create exe4.13.py

---
+++ exe4.13.py
@@ -0,0 +1,9 @@
+print()
+print('4.13 – Buffet:\n Um restaurante do tipo buffet oferece apenas cinco tipos básicos de comida. Pense em cinco pratos simples e armazene-os em uma tupla.\n• Use um laço for para exibir cada prato oferecido pelo restaurante.\n• Tente modificar um dos itens e cerifique-se de que Python rejeita a mudança.\n• O restaurante muda seu  cardápio, substituindo dois dos itens com pratos diferentes. Acrescente um bloco de código que reescreva a tupla e, em seguida, use um laço for para exibir cada um dos itens do cardápio revisado.')
+print()
+
+pratos = ('sopa', 'macarronada', 'Churrasco', 'feijoada', 'Cuscuz')
+for prato in pratos:
+    print(prato)
+
+    
