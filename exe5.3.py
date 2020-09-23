db19800d680894e68c88a346c979e1d25a6e89b4
João Correia <jcjunior.contatos@gmail.com>
2020-09-23 00:22:08 -0300

    Create exe5.3.py

---
+++ exe5.3.py
@@ -0,0 +1,24 @@
+print()
+print(
+    '5.3 – Cores de alienígenas #1:\n' 
+    'Suponha que um alienígena acabou de ser atingido em um ' 
+    'jogo. Crie uma variável chamada alien_color e atribua-lhe ' 
+    'um valor igual a "verde", "amarelo" ou "vermelho".\n'
+    '• Escreva uma instrução if para testar se a cor do '
+    'alienígena é verde. Se for,mostre uma mensagem informando ' 
+    'que o jogador acabou de ganhar cinco pontos.\n'
+    '• Escreva uma versão desse programa em que o teste if '
+    'passe e outro em que ele  falhe.  (A  versão  que  falha '
+    'não  terá  nenhuma  saída.)' 
+)
+print()
+
+alien_color = 'verde'
+if alien_color == 'verde':
+    print("Você ganhou 05 pontos!")
+
+alien_color = 'vermelho'
+if alien_color == 'verde':
+    print("Você ganhou 05 pontos!")
+
+
