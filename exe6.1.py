17064fbe40d752faabaf3732a98778c6b3233629
João Correia <jcjunior.contatos@gmail.com>
2020-09-26 01:26:29 -0300

    Create exe6.1.py

---
+++ exe6.1.py
@@ -0,0 +1,14 @@
+print()
+print(
+    '6.1 – Pessoa:\n'
+    ' Use um dicionário para armazenar informações sobre uma '
+    'pessoa que você conheça. Armazene seu primeiro nome, o '
+    'sobrenome, a idade e a cidade em que ela vive. Você deverá '
+    'ter chaves como first_name, last_name, age e city. Mostre '
+    'cada informação armazenada em seu dicionário'
+)
+print()
+
+pessoa = {'first_name': 'Ester', 'last_name': 'Francisca', 'age': 30, 'city': 'Aracaju'}
+
+print(pessoa)
