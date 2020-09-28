df7f4d6072b0c80b5603bd2a438731ecb46cc94c
João Correia <jcjunior.contatos@gmail.com>
2020-09-27 23:51:07 -0300

    Create exe6.2.py

---
+++ exe6.2.py
@@ -0,0 +1,15 @@
+print()
+print(
+    '6.2 – Números favoritos:\n'
+    ' Use um dicionário para armazenar os números favoritos '
+    'de algumas pessoas. Pense em cinco nomes e use-os como '
+    'chaves em seu dicionário. Pense em um número favorito para '
+    'cada pessoa e armazene cada um como um valor em seu '
+    'dicionário. Exiba o nome de cada pessoa e seu número '
+    'favorito. Para que seja mais divertido ainda, faça uma '
+    'enquete com alguns amigos e obtenha alguns dados reais '
+    'para o seu programa.'
+)
+print()
+amigos = {'Thiago':36, 'Angela':35, 'Ester':31, 'Matheus':22, 'Bernardo': 3}
+print(amigos)
