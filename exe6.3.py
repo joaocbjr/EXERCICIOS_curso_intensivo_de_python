9ffdd447e58bde9a0203af626afb64cf5b1d4021
João Correia <jcjunior.contatos@gmail.com>
2020-09-28 00:06:05 -0300

    exe6.3.py

---
+++ exe6.3.py
@@ -0,0 +1,20 @@
+print()
+print(
+    '6.3 – Glossário:\n'
+    ' Um dicionário Python pode ser usado para modelar um '
+    'dicionário de verdade. No entanto, para evitar confusão, '
+    'vamos chamá-lo de glossário.\n'
+    ' • Pense em cinco palavras relacionadas à programação '
+    'que você conheceu nos capítulos anteriores. Use essas '
+    'palavras como chaves em seu glossário e armazene seus '
+    'significados como valores.\n'
+    ' • Mostre cada palavra e seu significado em uma saída '
+    'formatada de modo elegante. Você pode exibir a palavra '
+    'seguida de dois-pontos e depois o seu significado, ou '
+    'apresentar a palavra em uma linha e então exibir seu '
+    'significado indentado em uma segunda linha. Utilize o '
+    'caractere de quebrade linha "(\n)" para inserir uma '
+    'linha em branco entre cada par palavra-significado em '
+    'sua saída.'
+)
+print()
