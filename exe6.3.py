a7fa342928cdf295d20e55e91df3a51d92a49cae
João Correia <jcjunior.contatos@gmail.com>
2020-09-29 00:12:44 -0300

    Update exe6.3.py

--- exe6.3.py
+++ exe6.3.py
@@ -13,8 +13,16 @@ print(
     'seguida de dois-pontos e depois o seu significado, ou '
     'apresentar a palavra em uma linha e então exibir seu '
     'significado indentado em uma segunda linha. Utilize o '
-    'caractere de quebrade linha "(\n)" para inserir uma '
+    'caractere de quebrade linha "( \ n )" para inserir uma '
     'linha em branco entre cada par palavra-significado em '
     'sua saída.'
 )
 print()
+glossario = {
+'if': 'altera o fluxo baseado no valor, verdadeiro ou falso, de uma expressão lógica.',
+'lista': 'é uma coleção de itens em uma ordem em particular.',
+'variaveis': 'é convencionalmente, um elemento representante do conjunto de todos os resultados possíveis de um fenômeno.',
+'strings': 'é simplesmente uma série de caracteres.',
+'tupla': 'é uma Lista imutável.'
+}
+
