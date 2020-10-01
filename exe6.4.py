cefc677b851ad658af3ef221a85c58e9f350b65f
João Correia <jcjunior.contatos@gmail.com>
2020-10-01 00:15:21 -0300

    exe6.4.py
    Glossário 2:

---
+++ exe6.4.py
@@ -0,0 +1,9 @@
+print('\n6.4 – Glossário 2:\n'
+    ' Agora que você já sabe como percorrer um dicionário comum '
+    'laço, limpe o código do Exercício 6.3 (página 148), '
+    'substituindo sua sequência de instruções print por um laço '
+    'que percorra as chaves e os valores do dicionário. Quando '
+    'tiver certeza de que seu laço funciona, acrescente mais cinco '
+    'termos de Python ao seu glossário. Ao executar seu programa '
+    'novamente, essas palavras e significados novos deverão ser '
+    'automaticamente incluídos na saída.\n')
