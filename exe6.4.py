2e8fc5e6e6f12c2446845504e74009d4ed2e603b
João Correia <jcjunior.contatos@gmail.com>
2020-10-02 00:24:52 -0300

    Update exe6.4.py
    Glossario 2

--- exe6.4.py
+++ exe6.4.py
@@ -7,3 +7,14 @@ print('\n6.4 – Glossário 2:\n'
     'termos de Python ao seu glossário. Ao executar seu programa '
     'novamente, essas palavras e significados novos deverão ser '
     'automaticamente incluídos na saída.\n')
+print("GLOSSÁRIO:")
+glossario = {
+    'if': 'altera o fluxo baseado no valor, verdadeiro ou falso, de uma expressão lógica.',
+    'lista': 'é uma coleção de itens em uma ordem em particular.',
+    'variaveis': 'é convencionalmente, um elemento representante do conjunto de todos os resultados possíveis de um fenômeno.',
+    'strings': 'é simplesmente uma série de caracteres.',
+    'tupla': 'é uma Lista imutável.'
+    }
+
+for verbete, signif in glossario.items():
+    print(verbete + ': ' + signif)
