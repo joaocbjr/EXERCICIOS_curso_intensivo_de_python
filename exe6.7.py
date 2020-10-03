0549c6f6b9738f0e2514d374091ecc789c61bb40
João Correia <jcjunior.contatos@gmail.com>
2020-10-02 23:59:09 -0300

    Create exe6.7.py

---
+++ exe6.7.py
@@ -0,0 +1,9 @@
+print('\n6.7 – Pessoas:\n'
+    ' Comece com o programa que você escreveu no Exercício '
+    '6.1(página 147). Crie dois novos dicionários que representem'
+    ' pessoas diferentes earmazene os três dicionários em uma '
+    'lista chamada people. Percorra sua listade pessoas com '
+    'um laço. À medida que percorrer a lista, apresente tudo '
+    'que você sabe sobre cada pessoa.\n')
+
+    
