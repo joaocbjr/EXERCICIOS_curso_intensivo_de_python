cc6d78a5068f57e537bc675798f2ef35e0ec9f07
João Correia <jcjunior.contatos@gmail.com>
2020-10-04 23:52:50 -0300

    Update exe6.7.py

--- exe6.7.py
+++ exe6.7.py
@@ -1,9 +1,13 @@
 print('\n6.7 – Pessoas:\n'
     ' Comece com o programa que você escreveu no Exercício '
     '6.1(página 147). Crie dois novos dicionários que representem'
-    ' pessoas diferentes earmazene os três dicionários em uma '
-    'lista chamada people. Percorra sua listade pessoas com '
+    ' pessoas diferentes e armazene os três dicionários em uma '
+    'lista chamada people. Percorra sua lista de pessoas com '
     'um laço. À medida que percorrer a lista, apresente tudo '
     'que você sabe sobre cada pessoa.\n')
 
-    
+pessoa_0 = {'first_name': 'Ester', 'last_name': 'Francisca', 'age': 32, 'city': 'Aracaju'}
+pessoa_1 = {'first_name': 'João', 'last_name': 'Correia', 'age': 38, 'city': 'Aracaju'}
+pessoa_2 = {'first_name': 'Bernardo', 'last_name': 'Sandes', 'age': 3, 'city': 'Aracaju'}
+
+people = [pessoa_0, pessoa_1, pessoa_2]
