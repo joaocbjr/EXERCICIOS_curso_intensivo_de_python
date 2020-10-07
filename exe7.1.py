ec760cb2244810afa250f00c3bf046e740c13848
João Correia <jcjunior.contatos@gmail.com>
2020-10-07 01:22:34 -0300

    Create exe7.1.py

---
+++ exe7.1.py
@@ -0,0 +1,8 @@
+print('\n7.1 – Locação de automóveis: \n'
+    'Escreva um programa que pergunte ao usuário qual tipo '
+    'de carro ele gostaria de alugar. Mostre uma mensagem '
+    'sobre esse carro, por exemplo, “Deixe-me ver se consigo '
+    'um Subaru para você.”')
+
+carro  = input('Qual carroa gostaria de alugar: ')
+print('\nDeixe-me ver se consigo um ' + carro +' pra você.\n')
