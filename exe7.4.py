7253d345b7564ec7e0c9968bc37a2e407665a54c
João Correia <jcjunior.contatos@gmail.com>
2020-10-09 23:48:22 -0300

    Create exe7.4.py

---
+++ exe7.4.py
@@ -0,0 +1,12 @@
+print('\n7.4 – Ingredientes para uma pizza:\n'
+' Escreva um laço que peça ao usuário para fornecer uma série '
+'de ingredientes para uma pizza até que o valor "quit" seja '
+'fornecido. À medida que cada ingrediente é especificado, '
+'apresente uma mensagem informando que você acrescentará esse '
+'ingrediente à pizza.\n')
+
+prompt = '\n(Adicione ingrediente a pizza)'
+while True:
+    city = input(prompt)
+    input('Adcione ingrediente a pizza: ')
+
