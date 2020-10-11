127dd66f10fc45b411a1cc34c5361dfd042a1946
João Correia <jcjunior.contatos@gmail.com>
2020-10-10 23:57:15 -0300

    Create exe7.5.py

---
+++ exe7.5.py
@@ -0,0 +1,10 @@
+print('\n7.5 – Ingressos para o cinema: \n'
+    ' Um cinema cobra preços diferentes para os ingressos de '
+    'acordo com a idade de uma pessoa. Se uma pessoa tiver '
+    'menos de 3 anos de idade, o ingresso será gratuito; se '
+    'tiver entre 3 e 12 anos, o ingresso custará 10 dólares; '
+    'se tiver mais de 12 anos, o ingresso custará 15 dólares. '
+    'Escreva um laço em que você pergunte a idade aos usuários '
+    'e, então,informe-lhes o preço do ingresso do cinema.\n')
+
+idade = int(input())
