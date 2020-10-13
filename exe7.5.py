8437255761dd6ad122d24c9298edc5626dfca91b
João Correia <jcjunior.contatos@gmail.com>
2020-10-12 23:54:05 -0300

    Update exe7.5.py

--- exe7.5.py
+++ exe7.5.py
@@ -7,4 +7,18 @@ print('\n7.5 – Ingressos para o cinema: \n'
     'Escreva um laço em que você pergunte a idade aos usuários '
     'e, então,informe-lhes o preço do ingresso do cinema.\n')
 
-idade = int(input())
+while True:
+    ingre = int(input('Quer comprar ingreço? [1 = Sim | 0 = Não]: '))
+    if ingre == 1:
+        idade = int(input('Informe a sua idade: '))
+        if idade < 3:
+            print('Ingresso gratuito\n')
+        elif idade < 12:
+            print('Ingresso custa US$ 10,00\n')
+        elif idade > 11:
+            print('Ingresso custa US$ 15,00\n') 
+        else:
+            print('Informe uma idade válida')
+    else:
+        break
+print('Obrigado, volte sempre!')
