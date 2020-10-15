d3bd14474ccd14d438d42853959ed2626260eae9
João Correia <jcjunior.contatos@gmail.com>
2020-10-14 23:44:43 -0300

    Update name.py

--- name.py
+++ name.py
@@ -36,7 +36,7 @@ while (True):
 
 media = sum(numlist) / len(numlist)
 print('Média: ',media)
-'''
+
 ingrediente = ['calabresa', 'parmesão', 'champion']
 
 if 'calabresa' in ingrediente:
@@ -45,3 +45,20 @@ if 'mussarela' in ingrediente:
 	print('adicione mussarela')
 if 'champion' in ingrediente:
 	print('adicione champion')
+'''
+# Começa com os usuários que precisam ser verificados,
+# e com uma lista vazia para armazenar os usuários confirmados 
+unconfirmed_users = ['alice', 'brian', 'candace', 'joão', 'ester']
+confirmed_users = []
+# Verifica cada usuário até que não haja mais usuários não confirmados 
+#Transfere cada usuário verificado para a lista de usuários confirmados
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print("Verificando usuário: " + current_user.title()) 
+    confirmed_users.append(current_user)
+# Exibe todos os usuários confirmados 
+print("\nOs seguintes usuários foram confirmados:") 
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
