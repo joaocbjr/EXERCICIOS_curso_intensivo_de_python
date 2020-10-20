a7ff47651114d3e389bcbc069a19ccf18cff785a
João Correia <jcjunior.contatos@gmail.com>
2020-10-20 00:22:02 -0200

    Create exe8.1.py

---
+++ exe8.1.py
@@ -0,0 +1,10 @@
+print('\n8.1 – Mensagem:\n'
+    ' Escreva uma função chamada display_message() que mostre '
+    'uma frase informando a todos o que você está aprendendo '
+    'neste capítulo. Chame a função e certifique-se de que a '
+    'mensagem seja exibida corretamente.\n')
+
+def display_message():
+    print('\tNeste capítulo aprenderemos a escrever funções, que são blocos de código nomeados, concebidos para realizar uma tarefa específica. Quando queremos executar uma tarefa em particular, definida em uma função, chamamos o nome da função responsável por ela.')
+
+display_message()
