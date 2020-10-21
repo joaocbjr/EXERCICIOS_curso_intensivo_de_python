48d5488bc8860eefe2779cb28b465badbf0ad494
João Correia <jcjunior.contatos@gmail.com>
2020-10-21 01:24:41 -0300

    Update exe8.3.py

--- exe8.3.py
+++ exe8.3.py
@@ -5,4 +5,16 @@ print('\n8.3 – Camiseta:\n'
     'o tamanho da camiseta e a mensagem estampada.\n\tChame a '
     'função uma vez usando argumentos posicionais para criar '
     'uma camiseta. Chame a função uma segunda vez usando argumentos '
-    'nomeados.')
+    'nomeados.\n')
+
+def make_shirt(tamanho, frase):
+    print('O tamanho da camiseta é ' + tamanho)
+    print('Com a frase: ' + frase)
+
+make_shirt('M', 'Jesus salva.\n')
+
+def make_shirt(tamanho = 'G', frase = 'O cordeiro venceu.'):
+    print('O tamanho da camiseta é ' + tamanho)
+    print('Com a frase: ' + frase)
+
+make_shirt()
