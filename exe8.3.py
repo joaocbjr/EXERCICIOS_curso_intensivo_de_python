d3f128f49d82768864f376e3abe1be5a342d5066
João Correia <jcjunior.contatos@gmail.com>
2020-10-20 23:32:29 -0300

    Create exe8.3.py

---
+++ exe8.3.py
@@ -0,0 +1,8 @@
+print('\n8.3 – Camiseta:\n'
+    'Escreva uma função chamada make_shirt() que aceite um '
+    'tamanho e o texto de uma mensagem que deverá ser estampada '
+    'na camiseta. A função deve exibir uma frase que mostre '
+    'o tamanho da camiseta e a mensagem estampada.\n\tChame a '
+    'função uma vez usando argumentos posicionais para criar '
+    'uma camiseta. Chame a função uma segunda vez usando argumentos '
+    'nomeados.')
