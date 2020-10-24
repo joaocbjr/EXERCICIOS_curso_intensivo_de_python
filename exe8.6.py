e8a1da0a9a29b6395b4e6d877bc8066bdffcc365
João Correia <jcjunior.contatos@gmail.com>
2020-10-23 23:55:36 -0300

    Create exe8.6.py
    Nomes de cidade

---
+++ exe8.6.py
@@ -0,0 +1,6 @@
+print('\n8.6 – Nomes de cidade:\n'
+    ' Escreva uma função chamada city_country() que aceite o '
+    'nome de uma cidade e seu país. A função deve devolver uma '
+    'string formatada assim: "Santiago, Chile"\n'
+    '\tChame sua função com pelo menos três pares cidade-país '
+    'e apresente o valor devolvido.\n')
