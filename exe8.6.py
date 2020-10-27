6c8ec7aed74d4c679190362c44b92032aceed7cc
João Correia <jcjunior.contatos@gmail.com>
2020-10-26 23:58:04 -0300

    Update exe8.6.py

--- exe8.6.py
+++ exe8.6.py
@@ -1,6 +1,17 @@
 print('\n8.6 – Nomes de cidade:\n'
     ' Escreva uma função chamada city_country() que aceite o '
     'nome de uma cidade e seu país. A função deve devolver uma '
-    'string formatada assim: "Santiago, Chile"\n'
-    '\tChame sua função com pelo menos três pares cidade-país '
+    'string formatada assim: "Santiago, Chile".\n'
+    'Chame sua função com pelo menos três pares cidade-país '
     'e apresente o valor devolvido.\n')
+
+def city_country():
+    #Devolve cidade, país.
+    while True:
+        if city == s:
+            city = str(input('Deseja inserir mais cidades [s/n]:'))
+            dado = str(input('Digite uma cidade: ', 'e o país: '))
+        else:
+            print(dado)
+        
+city_country()
